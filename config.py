b24264c13a2e13b63adcbc68eb40f8a6c9171c36
himesh <buchhimesh@gmail.com>
2020-06-08 15:26:34 -0400

    added instruction

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
-# url for yahoo finance's website
-URL = "https://finance.yahoo.com/gainers"
-TOPNUM = 2
-TOPNUM_CHANGE = 10
-INIT_WEALTH = 1000
-COUNT_WEALTH = 1000
-LOWBUY1 = 12
-LOWBUY2 = 40
-TWO_CHECKS = False
-LOWBUY = 15
+URL = "https://finance.yahoo.com/gainers"  # url for yahoo finance's website
+TOPNUM = 2  # if the change amount is up by what it was + TOPNUM then we sell the stock
+TOPNUM_CHANGE = 10  # this is only used in top_changed() method, to get the stocks who have chnaged >=TOPNUM_CHANGED
+INIT_WEALTH = 1000  # the amount you start off with
+LOWBUY1 = 12  # if TWOCHECKS is true then we will buy the stocks that have changed between LOWBUY1 and LOWBUY2
+LOWBUY2 = 40  # if TWOCHECKS is true then we will buy the stocks that have changed between LOWBUY1 and LOWBUY2
+TWO_CHECKS = False  # determine whether to buy stocks that have changed amount in an interval or no
+LOWBUY = 15  # if TWOCHECKS is set to false, then we will buy stocks that have changed amount >=LOWBUY
